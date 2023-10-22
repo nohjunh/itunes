a35ba0d5111acc212d83a0c94c93dd47281b4b9c
Junhyeok <njh042777@gmail.com>
2023-10-22 11:52:15 +0900

    chore : Gradle Kotlin Convention Plugin 정의

---
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,43 @@
+import com.nohjunh.android.watcha.assignment.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.kotlin
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            dependencies {
+                add("implementation", project(":core:model"))
+                add("implementation", project(":core:ui"))
+                add("implementation", project(":core:designsystem"))
+                add("implementation", project(":core:data"))
+                add("implementation", project(":core:common"))
+                add("implementation", project(":core:domain"))
+
+                add("testImplementation", kotlin("test"))
+                add("androidTestImplementation", kotlin("test"))
+
+                add("implementation", libs.findLibrary("lottie.compose").get())
+
+                add("implementation", libs.findLibrary("coil.kt").get())
+                add("implementation", libs.findLibrary("coil.kt.compose").get())
+                add("implementation", libs.findLibrary("coil.kt.gif").get())
+
+                // BottomNavigation 및 BottomNavigationItem 구성요소 사용
+                add("implementation", libs.findLibrary("androidx.compose.material.core").get())
+
+                add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
+
+                add("implementation", libs.findLibrary("kotlinx.coroutines.android").get())
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,19 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
+        maven(url = "https://jitpack.io")
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://jitpack.io")
     }
 }
 
