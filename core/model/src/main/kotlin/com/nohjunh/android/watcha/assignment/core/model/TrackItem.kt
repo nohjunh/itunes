2f2c6cfacadb2993e2abe12c7acf90c1678fdd16
Junhyeok <njh042777@gmail.com>
2023-10-23 01:48:35 +0900

    feat : core:model TrackItem 정의

---
+++ core/model/src/main/kotlin/com/nohjunh/android/watcha/assignment/core/model/TrackItem.kt
@@ -0,0 +1,9 @@
+package com.nohjunh.android.watcha.assignment.core.model
+
+data class TrackItem(
+    val id: Long,
+    val artistName: String,
+    val artworkUrl60: String,
+    val collectionName: String,
+    val trackName: String,
+)
