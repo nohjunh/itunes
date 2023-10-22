2539ebd288d163435424e89bc52485b13c76be9c
Junhyeok <njh042777@gmail.com>
2023-10-22 12:07:26 +0900

    chore : core:network Module 생성 및 build.gradle 셋팅

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -2,27 +2,39 @@ plugins {
     id("nohjunh.android.library")
     id("nohjunh.android.hilt")
     id("kotlinx-serialization")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
-    namespace = "com.nohjunh.android.watcha.core.data"
+    namespace = "com.nohjunh.android.watcha.core.network"
+    buildFeatures {
+        buildConfig = true
+    }
     testOptions {
         unitTests {
             isIncludeAndroidResources = true
-            isReturnDefaultValues = true
         }
     }
+}
+
+secrets {
+    defaultPropertiesFileName = "local.properties"
 }
 
 dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:model"))
-    implementation(project(":core:network"))
 
-    implementation(libs.androidx.core.ktx)
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.svg)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.datetime)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.gson)
+    implementation(libs.retrofit.kotlin.serialization)
     implementation(libs.paging.ktx)
     implementation(libs.paging.compose)
+
 }
