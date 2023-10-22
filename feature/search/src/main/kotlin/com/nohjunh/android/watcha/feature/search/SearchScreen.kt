15a05ff9572534a7006b762f3788360e7a1737a2
Junhyeok <njh042777@gmail.com>
2023-10-22 14:10:14 +0900

    chore : feature:search Module 생성 및 build.gradle 셋팅

---
+++ feature/search/src/main/kotlin/com/nohjunh/android/watcha/feature/search/SearchScreen.kt
@@ -0,0 +1,21 @@
+package com.nohjunh.android.watcha.feature.search
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SearchScreen(
+    modifier: Modifier = Modifier,
+    onShowSnackbar: (String) -> Unit,
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Search"
+        )
+    }
+}
