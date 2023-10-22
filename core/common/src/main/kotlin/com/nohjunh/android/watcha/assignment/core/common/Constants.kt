97ee717a36fef6d2669fb73d4c547ee6594fa029
Junhyeok <njh042777@gmail.com>
2023-10-22 16:15:06 +0900

    feat : BASE_URL Constants 정의

---
+++ core/common/src/main/kotlin/com/nohjunh/android/watcha/assignment/core/common/Constants.kt
@@ -0,0 +1,8 @@
+package com.nohjunh.android.watcha.assignment.core.common
+
+import com.nohjunh.android.watcha.core.common.BuildConfig
+
+object Constants {
+    const val BASE_URL = BuildConfig.BASE_URL
+
+}
