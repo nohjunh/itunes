33192ca3cd6aafb3cecc750da94f24a319a8c6eb
Junhyeok <njh042777@gmail.com>
2023-10-23 01:45:31 +0900

    fix : collectionId -> collectionName @SerializedName 수정

--- core/network/src/main/kotlin/com/nohjunh/android/watcha/assignment/core/network/model/response/TrackItemResponse.kt
+++ core/network/src/main/kotlin/com/nohjunh/android/watcha/assignment/core/network/model/response/TrackItemResponse.kt
@@ -2,14 +2,28 @@ package com.nohjunh.android.watcha.assignment.core.network.model.response
 
 
 import com.google.gson.annotations.SerializedName
+import com.nohjunh.android.watcha.assignment.core.database.model.TrackItemEntity
 
 data class TrackItemResponse(
+    @SerializedName("trackId")
+    val trackId: Long?,
     @SerializedName("artistName")
     val artistName: String?,
     @SerializedName("artworkUrl60")
     val artworkUrl60: String?,
-    @SerializedName("collectionId")
+    @SerializedName("collectionName")
     val collectionName: String?,
     @SerializedName("trackName")
     val trackName: String?,
 )
+
+fun TrackItemResponse.toEntity(offsetId: Int): TrackItemEntity {
+    return TrackItemEntity(
+        trackId = trackId ?: 0,
+        artistName = artistName ?: "default artistName",
+        artworkUrl60 = artworkUrl60 ?: "default Url",
+        collectionName = collectionName ?: "default collectionName",
+        trackName = trackName ?: "default trackName",
+        offsetId = offsetId
+    )
+}
