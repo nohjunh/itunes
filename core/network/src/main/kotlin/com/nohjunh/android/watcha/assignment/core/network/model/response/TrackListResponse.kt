1947e79cf833e6e6bec201e27b85cb30561faea1
Junhyeok <njh042777@gmail.com>
2023-10-23 01:44:49 +0900

    refactor : TrackResponse -> TrackListResponse 네이밍 변경

--- core/network/src/main/kotlin/com/nohjunh/android/watcha/assignment/core/network/model/response/TrackListResponse.kt
+++ core/network/src/main/kotlin/com/nohjunh/android/watcha/assignment/core/network/model/response/TrackListResponse.kt
@@ -3,7 +3,7 @@ package com.nohjunh.android.watcha.assignment.core.network.model.response
 
 import com.google.gson.annotations.SerializedName
 
-data class TrackResponse(
+data class TrackListResponse(
     @SerializedName("resultCount")
     val resultCount: Int?,
     @SerializedName("results")
