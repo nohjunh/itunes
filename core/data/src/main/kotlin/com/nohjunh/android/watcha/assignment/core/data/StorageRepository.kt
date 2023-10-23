04baf235ab89ad29e39237113edae37bd88d071d
Junhyeok <njh042777@gmail.com>
2023-10-23 17:08:20 +0900

    feat : 보관함에 저장된 TrackItem 관리를 위한 StorageRepository 추가 및 로직 반영

---
+++ core/data/src/main/kotlin/com/nohjunh/android/watcha/assignment/core/data/StorageRepository.kt
@@ -0,0 +1,11 @@
+package com.nohjunh.android.watcha.assignment.core.data
+
+import com.nohjunh.android.watcha.assignment.core.common.result.Result
+import com.nohjunh.android.watcha.assignment.core.model.TrackItem
+import kotlinx.coroutines.flow.Flow
+
+interface StorageRepository {
+    suspend fun getStorageTrackList(): Flow<Result<List<TrackItem>>>
+    suspend fun deleteTrackItem(trackId: Long): Flow<Result<Unit>>
+
+}
--- core/data/src/main/kotlin/com/nohjunh/android/watcha/assignment/core/data/di/DataModule.kt
+++ core/data/src/main/kotlin/com/nohjunh/android/watcha/assignment/core/data/di/DataModule.kt
@@ -2,6 +2,8 @@ package com.nohjunh.android.watcha.assignment.core.data.di
 
 import com.nohjunh.android.watcha.assignment.core.data.SearchRepository
 import com.nohjunh.android.watcha.assignment.core.data.SearchRepositoryImpl
+import com.nohjunh.android.watcha.assignment.core.data.StorageRepository
+import com.nohjunh.android.watcha.assignment.core.data.StorageRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -16,5 +18,11 @@ internal interface DataModule {
     fun bindSearchRepository(
         searchRepository: SearchRepositoryImpl,
     ): SearchRepository
+
+    @Singleton
+    @Binds
+    fun bindStorageRepository(
+        storageRepository: StorageRepositoryImpl,
+    ): StorageRepository
 
 }
