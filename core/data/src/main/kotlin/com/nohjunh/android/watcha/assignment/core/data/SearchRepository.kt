10b62ce943411f1762706c29785b72579cf820ec
Junhyeok <njh042777@gmail.com>
2023-10-23 17:07:30 +0900

    feat : 선택한 TrackItem 저장을 위한 SearchRepository 로직 추가

--- core/data/src/main/kotlin/com/nohjunh/android/watcha/assignment/core/data/SearchRepository.kt
+++ core/data/src/main/kotlin/com/nohjunh/android/watcha/assignment/core/data/SearchRepository.kt
@@ -3,9 +3,10 @@ package com.nohjunh.android.watcha.assignment.core.data
 import androidx.paging.PagingData
 import com.nohjunh.android.watcha.assignment.core.model.TrackItem
 import kotlinx.coroutines.flow.Flow
+import com.nohjunh.android.watcha.assignment.core.common.result.Result
 
 interface SearchRepository {
     fun getTrackList(): Flow<PagingData<TrackItem>>
-    suspend fun deleteTrackList()
+    suspend fun saveTrackItem(trackItem: TrackItem): Flow<Result<Unit>>
 
 }
